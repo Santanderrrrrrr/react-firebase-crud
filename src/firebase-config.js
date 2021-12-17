e65e6dd89cdfaa03f59e713a0289532cacc0558d
Leon Omollo <57505259+Santanderrrrrrr@users.noreply.github.com>
2021-12-17 17:16:34 +0100

    Edit firebase-config.js
    Personal configuration information needed

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,14 +4,15 @@ import { getFirestore} from "@firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCbTWwujX1bRHmew2piGTdKVPkeYNSri1E",
-    authDomain: "instagramcloneapp-react.firebaseapp.com",
-    projectId: "instagramcloneapp-react",
-    storageBucket: "instagramcloneapp-react.appspot.com",
-    messagingSenderId: "523333922948",
-    appId: "1:523333922948:web:6c7d621f45715dc38dd2d1",
-    measurementId: "G-906G81J7D9"
+    apiKey: "#on request",
+    authDomain: "#",
+    projectId: "#",
+    storageBucket: "#",
+    messagingSenderId: "#",
+    appId: "#",
+    measurementId: "#"
   };
+//firebaseConfig is unique to user. Create firebase collection and paste your config information into the spaces above.
 
 
   const app = initializeApp(firebaseConfig);
